44971ec2e4921db4a1c2acdc83d1293d99cb7598
Akhmad Lylana <57022642+JAMBROONG@users.noreply.github.com>
2021-03-22 00:01:57 +0700

    Update bawah.php

--- praktikum 2-2/bawah.php
+++ praktikum 2-2/bawah.php
@@ -1,5 +1,13 @@
 <footer><p>Created by <a href="http://nurulfikri.ac.id">Pusinfo NF &copy;2017</a></p></footer>
 </div>
 </body>
-
+<script>
+		function hanyaAngka(evt) {
+		  var charCode = (evt.which) ? evt.which : event.keyCode
+		   if (charCode > 31 && (charCode < 48 || charCode > 57))
+ 
+		    return false;
+		  return true;
+		}
+	</script>
 </html>
